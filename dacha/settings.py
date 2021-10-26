543935c96108597198fdee9aa82a7ca876ac5c87
Uktamjon <uktamforjob@gmail.com>
2021-10-26 15:05:43 +0500

    cors headers changed

--- dacha/settings.py
+++ dacha/settings.py
@@ -114,9 +114,9 @@ REDIS_PORT = 6379
 # }
 
 CORS_ALLOWED_ORIGINS = [
-    # "localhost:3000",
-    # "http://localhost:3000",
-    # "https://dachaturizm.netlify.app/",
-    # "http://dachaturizm.netlify.app/",
-    # "dachaturizm.netlify.app/"
+    "localhost:3000",
+    "http://localhost:3000",
+    "https://dachaturizm.netlify.app/",
+    "http://dachaturizm.netlify.app/",
+    "dachaturizm.netlify.app/"
 ]
